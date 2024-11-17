0a3ad80f409d151c86988b859410187794fb4c15
Nehali <bhaleraonehali7@gmail.com>
2024-11-17 21:30:02 +0530

    employme management update

--- employ-db-management/src/App.jsx
+++ employ-db-management/src/App.jsx
@@ -1,35 +1,79 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
+// id,uname,address,email,mobile,DOB
 function App() {
-  const [count, setCount] = useState(0)
 
+  const[formData,setformData]=useState({
+    username:"",
+    email:"",
+    address:""
+
+  })
+
+  const[data,setdata]=useState([
+    // {id:Date.now(),imgurl:"",uname:"neha",address:"Puen",email:"neha@gmail.com",}
+  ])
+
+
+  function handleChange(e){
+    const{name,value}=e.target;
+    setformData((prev)=>{
+      return {
+        ...prev,
+        [name]:value
+      }
+    })
+  }
+
+
+
+  function addData(){
+    setdata((prev)=>[...prev,formData])
+    setformData({
+      address:"",
+      email:"",
+      username:""
+    })    
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='h-screen	w-screen p-11'>
+
+    <div className='flex justify-between '>
+    <h1 className='text-xl'>Employee Database management</h1>
+    {/* <button className='bg-slate-300 p-4 w-100 '>Add New</button> */}
+    </div>
+   <h1>Form</h1>
+   <input placeholder='name' name='username' onChange={handleChange} value={formData.username}/>
+   <input placeholder='address' name='address' onChange={handleChange}  value={formData.address}/>
+   <input placeholder='email' name='email' onChange={handleChange}  value={formData.email}/>
+    <button className='bg-slate-300 p-4 w-100 ' onClick={addData}>Add</button>
+
+   <div className='flex '>
+    <div>
+    <h1>Employ list</h1>
+    <ol>
+    {
+      data?.map((i)=>{
+        return <li key={i.id}>
+            {i.username}   {i.email}
+        </li>
+      })
+    }
+    </ol>
+    
+    </div>
+    
+    <div>
+    <h1>Employ Description</h1>
+    </div>
+    
+   </div>
+    </div>
   )
 }
 
 export default App
+
+
